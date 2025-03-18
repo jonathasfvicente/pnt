e8aab0e4d12ea7f52d561af32a8b2a102202bb05
jonathasfvicente <jonathasfelipe2580@gmail.com>
2025-03-18 15:48:34 -0300

    Add files via upload

---
+++ tulinhomaravilha.py
@@ -0,0 +1,6 @@
+protagonista = input(print('Qual o protagonista da história? '))
+feito = input(print('Qual o feito dele? '))
+onde = input(print('Qual o lugar do feito? '))
+cumpanhero = input(print('Qual o companheiro dele? '))
+
+print(f'{protagonista} estava {onde} com {cumpanhero}, quando de repente ele {feito}.')
